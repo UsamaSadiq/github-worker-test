b2b9a3d5e2feb97dbf18bddc16398a7a0762cac7
Usama Sadiq <usama.sadiq@arbisoft.com>
2021-06-15 11:44:55 +0500

    Create action-test.py

---
+++ .github/workflows/action-test.py
@@ -0,0 +1,11 @@
+from colorama import Fore, Back, Style
+
+print(Fore.RED + 'Initializing the Script.\n')
+
+print(Back.GREEN + 'Script is running fine.\n')
+
+print(Style.DIM + 'Script Completed.\n')
+
+print(Style.RESET_ALL)
+
+print('Colorama Deactivated.')
